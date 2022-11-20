8b83448d0c4c8ed07027706e5108e112a785fbc4
Christo du Toit <christo@dutoit.co.uk>
2022-11-20 22:28:12 +0000

    ShouldVerifyThatAllTemplateDeserialise -> PASS

---
+++ Standardly.Templates.Tests.Acceptance/TemplateValidationTests.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+using Standardly.Core.Brokers.Files;
+using Standardly.Core.Brokers.Loggings;
+using Standardly.Core.Brokers.RegularExpressions;
+using Standardly.Core.Models.Configurations.Retries;
+using Standardly.Core.Services.Foundations.Files;
+using Standardly.Core.Services.Foundations.Templates;
+
+namespace Standardly.Templates.Tests.Acceptance
+{
+    public partial class TemplateValidationTests
+    {
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IFileService fileService;
+        private readonly ITemplateService templateService;
+
+
+
+        public TemplateValidationTests()
+        {
+            string assembly = Assembly.GetExecutingAssembly().Location;
+            string templateFolderPath = Path.Combine(Path.GetDirectoryName(assembly), @"Templates");
+            string templateDefinitionFileName = "Template.json";
+            this.loggingBroker = InitialiseLogger();
+
+            this.fileService = new FileService(
+                fileBroker: new FileBroker(),
+                retryConfig: new RetryConfig(),
+                loggingBroker: this.loggingBroker);
+
+            this.templateService = new TemplateService(
+                fileBroker: new FileBroker(),
+                regularExpressionBroker: new RegularExpressionBroker(),
+                loggingBroker: this.loggingBroker);
+        }
+
+        private ILoggingBroker InitialiseLogger()
+        {
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("Standardly", LogLevel.Warning);
+            });
+
+            ILogger<LoggingBroker> logger = loggerFactory.CreateLogger<LoggingBroker>();
+            return new LoggingBroker(logger);
+        }
+    }
+}
