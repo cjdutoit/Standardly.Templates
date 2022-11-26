afa6ec2a359fc8c547929613ff525c63d6935fc0
Christo du Toit <christo@dutoit.co.uk>
2022-11-26 18:07:20 +0000

    CODE RUB Code Cleanup

--- Standardly.Templates.Tests.Acceptance/TemplateValidationTests.Validations.cs
+++ Standardly.Templates.Tests.Acceptance/TemplateValidationTests.Validations.cs
@@ -8,12 +8,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 using Standardly.Core.Models.Foundations.Templates;
 using Standardly.Core.Models.Foundations.Templates.Exceptions;
-using Standardly.Core.Models.Processings.Templates.Exceptions;
 using Xunit;
 
 namespace Standardly.Templates.Tests.Acceptance
@@ -57,67 +55,6 @@ namespace Standardly.Templates.Tests.Acceptance
             }
 
             Assert.False(errorList.Length > 0, errorList.ToString());
-        }
-
-        [Fact]
-        public void ShouldVerifyThatAllVariableHasBeenReplacedAndTemplatesSavedInActualOutputFolder()
-        {
-            // given
-            InvalidReplacementTemplateException invalidReplacementTemplateException =
-                new InvalidReplacementTemplateException();
-
-            Dictionary<string, string> replacementDictionary = GetReplacementDictionary();
-            List<Template> templates = this.standardlyTemplateClient.FindAllTemplates();
-
-            // when
-            for (int templateCounter = 0; templateCounter <= templates.Count - 1; templateCounter++)
-            {
-                Template template = templates[templateCounter];
-                try
-                {
-                    for (int taskIndex = 0; taskIndex <= template.Tasks.Count - 1; taskIndex++)
-                    {
-                        var transformedTemplate =
-                            this.templateProcessingService
-                                .TransformTemplate(template, replacementDictionary);
-
-                        transformedTemplate.Tasks[taskIndex].Actions.ForEach(action =>
-                        {
-                            this.PerformFileCreations(action.Files, replacementDictionary);
-                            this.PerformAppendOpperations(action.Appends, replacementDictionary);
-                        });
-                    }
-                }
-                catch (TemplateProcessingDependencyValidationException templateProcessingDependencyValidationException)
-                {
-                    var templateName = template.Name ?? templateCounter.ToString();
-
-                    foreach (DictionaryEntry dictionaryEntry in
-                        templateProcessingDependencyValidationException.InnerException.Data)
-                    {
-                        invalidReplacementTemplateException.Data
-                            .Add($"Template[{templateName}].{dictionaryEntry.Key}", dictionaryEntry.Value);
-                    }
-                }
-            }
-
-            // then
-            if (invalidReplacementTemplateException.Data.Count > 0)
-            {
-                StringBuilder errorMessages = new StringBuilder();
-                errorMessages.AppendLine("Found the following unexpected tags:" + Environment.NewLine);
-
-
-                foreach (DictionaryEntry dictionaryEntry in invalidReplacementTemplateException.Data)
-                {
-                    string errors = ((List<string>)dictionaryEntry.Value)
-                           .Select(value => value).Aggregate((t1, t2) => t1 + $"{Environment.NewLine}" + t2);
-
-                    errorMessages.AppendLine($"{dictionaryEntry.Key}");
-                }
-
-                Assert.Fail(errorMessages.ToString());
-            }
         }
     }
 }
