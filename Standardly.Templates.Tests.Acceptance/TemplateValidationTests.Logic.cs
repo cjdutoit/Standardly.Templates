9ccc2e77491d9331f4fd8c5295be20a99f80fcd0
Christo du Toit <christo@dutoit.co.uk>
2022-11-28 23:44:02 +0000

    CONTROLLERS: Controller Template

--- Standardly.Templates.Tests.Acceptance/TemplateValidationTests.Logic.cs
+++ Standardly.Templates.Tests.Acceptance/TemplateValidationTests.Logic.cs
@@ -69,6 +69,27 @@ namespace Standardly.Templates.Tests.Acceptance
 
                 Assert.Fail(errorMessages.ToString());
             }
+            catch (StandardlyClientDependencyException dependencyException)
+            {
+                StringBuilder errorMessages = new StringBuilder();
+                errorMessages.AppendLine($"{dependencyException.InnerException.Message}.  "
+                    + $"{dependencyException.InnerException?.InnerException?.Message}");
+
+                if (dependencyException.InnerException.Data.Count > 0)
+                {
+                    errorMessages.AppendLine("Found the following errors:" + Environment.NewLine);
+
+                    foreach (DictionaryEntry dictionaryEntry in dependencyException.InnerException.Data)
+                    {
+                        string errors = ((List<string>)dictionaryEntry.Value)
+                               .Select(value => value).Aggregate((t1, t2) => t1 + $"{Environment.NewLine}" + t2);
+
+                        errorMessages.AppendLine($"{dictionaryEntry.Key}");
+                    }
+                }
+
+                Assert.Fail(errorMessages.ToString());
+            }
             catch (Exception ex)
             {
                 Assert.Fail(ex.Message);
